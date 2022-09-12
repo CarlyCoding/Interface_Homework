c284cdcfe5b187c33457fd43c2e81818a598892d
CarlyCoding <carly_off@outlook.com>
2022-09-12 20:29:38 +0100

    All MVP completed

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -2,9 +2,10 @@ package attractions;
 
 import behaviours.IReviewed;
 import behaviours.ISecurity;
+import behaviours.ITicketed;
 import people.Visitor;
 
-public class RollerCoaster extends Attraction implements ISecurity, IReviewed{
+public class RollerCoaster extends Attraction implements ISecurity, IReviewed, ITicketed {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
@@ -17,5 +18,20 @@ public class RollerCoaster extends Attraction implements ISecurity, IReviewed{
         }
         else return false;
         }
+
+    @Override
+    public double defaultPrice() {
+        return 8.40;
     }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        if(visitor.getHeight() > 200){
+            return defaultPrice() * 2;
+        }
+        else {
+            return defaultPrice();
+        }
+    }
+}
 
